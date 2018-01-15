3bb2fe58f1177594e9b13f9ef7e7d0db7bc7b2b1
Stephen Geller <stephen.geller@hotmail.com>
2018-01-15 17:53:56 +0000

    First commit, setup dependencies & skeleton router

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import Properties from './Properties'
+import { BrowserRouter, Route } from 'react-router-dom';
+const Logout = () => <h2>Logged out</h2>;
+
+class App extends Component {
+    // componentDidMount == do something AFTER the component is rendered
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/properties" component={Properties} />
+                        <Route path="/logout" component={Logout} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default App;
