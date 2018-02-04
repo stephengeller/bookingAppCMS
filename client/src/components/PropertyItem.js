c078bd223e01a2b05d6464b99cb7cef374105d4c
stephengeller <stephen.geller@hotmail.com>
2018-02-04 20:53:11 +0000

    Added some styling and extra display to property items

--- client/src/components/PropertyItem.js
+++ client/src/components/PropertyItem.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class PropertyItem extends Component {
-
   render() {
-    let property = this.props.property
+    let property = this.props.property;
     return (
       <section className="property-item">
-          <h5>{property.title}</h5>
-          <h6 className='property-line'><span className="property-line-title">Description: </span>{property.description}</h6>
-          <div className='property-line'><span className="property-line-title">OwnerID: </span>{property.ownerId}</div>
-          <div className='property-line'><span className="property-line-title">Facilities: </span>{property.facilities}</div>
-          <button className="btn waves-effect waves-light" onClick={() => this.props.deleteProperty(property)}>Delete This Property</button>
-          <br/>
-          <br/>
+        <h5>{property.title}</h5>
+        <h6 className="property-line">
+          <span className="property-line-title">Description: </span>
+          {property.description}
+        </h6>
+        <div className="property-line">
+          <span className="property-line-title">OwnerID: </span>
+          {property.ownerId}
+        </div>
+        <div className="property-line">
+          <span className="property-line-title">Facilities: </span>
+          {property.facilities}
+        </div>
+        <button
+          className="btn waves-effect waves-light"
+          onClick={() => this.props.deleteProperty(property)}
+        >
+          Delete This Property
+        </button>
+        <br />
+        <br />
       </section>
-    )
+    );
   }
 }
 
-export default PropertyItem
+export default PropertyItem;
--- client/src/components/PropertyManager.js
+++ client/src/components/PropertyManager.js
@@ -1,68 +1,79 @@
 import React, { Component } from 'react';
-import PropertyForm from './PropertyForm'
-import PropertyItem from '../components/PropertyItem'
-import axios from 'axios'
-
+import PropertyForm from './PropertyForm';
+import PropertyItem from '../components/PropertyItem';
+import axios from 'axios';
 
 class ApiPropertyManager extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: 'Stephen',
-            properties: []
-        };
-        this.deleteProperty = this.deleteProperty.bind(this)
-        this.getProperties = this.getProperties.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'Stephen',
+      properties: []
+    };
+    this.deleteProperty = this.deleteProperty.bind(this);
+    this.getProperties = this.getProperties.bind(this);
+  }
 
-    deleteProperty(property) {
-      console.log('attempting to delete property')
-      axios
-          .delete("http://localhost:3000/properties/" + property.id)
-          .then((response) => {
-            console.log('successful delete of id: ' + property.id)
-            this.getProperties()
-          })
-          .catch(function(error) {
-              console.log('delete error: ', error);
-          })
-    }
+  deleteProperty(property) {
+    console.log('attempting to delete property');
+    axios
+      .delete('http://localhost:3000/properties/' + property.id)
+      .then(response => {
+        console.log('successful delete of id: ' + property.id);
+        this.getProperties();
+      })
+      .catch(function(error) {
+        console.log('delete error: ', error);
+      });
+  }
 
-    getProperties() {
-      axios
-          .get("http://localhost:3000/properties/search")
-          .then((response) => {
-            const properties = response.data
-              this.setState({ properties });
-              return properties
-          })
-          .catch(function(error) {
-              console.log(error);
-          })
-    }
+  getProperties() {
+    axios
+      .get('http://localhost:3000/properties/search')
+      .then(response => {
+        const properties = response.data;
+        this.setState({ properties });
+        return properties;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-    componentDidMount() {
-        this.getProperties()
-    }
+  componentDidMount() {
+    this.getProperties();
+  }
 
-    render() {
-      let properties = this.state.properties.reverse();
-      let counter = 0
-      return (
-          <div className='container'>
-              <h2 className="center-align">PropertiesApi</h2>
-              < PropertyForm getProperties={this.getProperties} properties={this.state.properties}/>
-              {properties.map(property => {
-                  counter += 1
-                  return (
-                      < PropertyItem property={property} key={counter} deleteProperty={this.deleteProperty} getProperties={this.getProperties}/>
-                    )
-                  })
-              }
-              <button className="btn waves-effect waves-light" onClick={() => this.getProperties()}>Get Properties</button>
-          </div>
-      );
-    }
+  render() {
+    let properties = this.state.properties.reverse();
+    let counter = 0;
+    return (
+      <div className="container">
+        <h2 className="center-align">PropertiesApi</h2>
+        <PropertyForm
+          getProperties={this.getProperties}
+          properties={this.state.properties}
+        />
+        {properties.map(property => {
+          counter += 1;
+          return (
+            <PropertyItem
+              property={property}
+              key={counter}
+              deleteProperty={this.deleteProperty}
+              getProperties={this.getProperties}
+            />
+          );
+        })}
+        <button
+          className="btn waves-effect waves-light"
+          onClick={() => this.getProperties()}
+        >
+          Get Properties
+        </button>
+      </div>
+    );
+  }
 }
 
 export default ApiPropertyManager;
