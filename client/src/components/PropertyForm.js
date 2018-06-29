68887c6126fb6d26e25d785ba01e527b89cc43ce
stephengeller <stephen.geller@hotmail.com>
2018-06-29 11:42:12 +0100

    refactor(property-form): Extract data to json files & loop over them.
    Also rename variables and cleanup other bits.

--- client/src/components/PropertyForm.js
+++ client/src/components/PropertyForm.js
@@ -7,209 +7,163 @@ import GoogleMapsAPI from '../modules/GoogleMapsAPI';
 import AddPropertyButton from './buttons/AddPropertyButton';
 import FormItem from './FormItem';
 
+const PropertyFields = require('../json/PropertyForm/PropertyFormFields');
+const RequiredFields = require('../json/PropertyForm/RequiredFields');
+const AllFields = require('../json/PropertyForm/AllFields');
+
 class PropertyForm extends Component {
-  constructor(props) {
-    super(props);
-    this.addProperty = this.addProperty.bind(this);
-    this.updateInputValue = this.updateInputValue.bind(this);
-    this.submissionInProgress = this.submissionInProgress.bind(this);
-    this.handleMissingFields = this.handleMissingFields.bind(this);
-    this.formatter = new Formatter();
-    this.errorHandler = new ErrorHandler();
-    this.mapsAPI = new GoogleMapsAPI({
-      googleApiKey: this.props.googleApiKey
-    });
-    this.allFields = [
-      'title',
-      'description',
-      'facilities',
-      'addressLine1',
-      'addressLine2',
-      'city',
-      'postcode',
+	constructor(props) {
+		super(props);
+		this.addProperty = this.addProperty.bind(this);
+		this.updateInputValue = this.updateInputValue.bind(this);
+		this.submissionInProgress = this.submissionInProgress.bind(this);
+		this.handleMissingFields = this.handleMissingFields.bind(this);
+		this.buildForm = this.buildForm.bind(this);
+		this.formatter = new Formatter();
+		this.errorHandler = new ErrorHandler();
+		this.mapsAPI = new GoogleMapsAPI({
+			googleApiKey: this.props.googleApiKey
+		});
+		this.allFields = AllFields;
+		this.requiredFields = RequiredFields;
 
-      'bookingEmail'
-    ];
-    this.requiredFields = [
-      'title',
-      'description',
-      'facilities',
-      'addressLine1',
-      'city',
-      'postcode',
+		this.state = {
+			fields: {
+				location: {}
+			},
+			notice: '',
+			loading: false
+		};
+	}
 
-      'bookingEmail'
-    ];
+	async setUpFieldsObject(fields) {
+		const { postcode } = this.state.fields;
+		const lngLat = await this.mapsAPI.getPostcodeResults(postcode);
+		const { title, description, bookingEmail } = fields;
+		return {
+			title,
+			description,
+			location: {
+				lat: lngLat.latitude,
+				lon: lngLat.longitude
+			},
+			facilities: this.formatter.formatItemStringToArray(fields.facilities),
+			address: await this.formatter.convertAddressToArray(fields),
+			ownerId: 'testOwnerId',
+			bookingEmail
+		};
+	}
 
-    this.state = {
-      fields: {
-        location: {}
-      },
-      error: '',
-      loading: false
-    };
-  }
+	cleanFields(fieldsToClean, currentFieldsState) {
+		fieldsToClean.map(fieldName => (currentFieldsState[fieldName] = ''));
+		return currentFieldsState;
+	}
 
-  async setUpFieldsObject(fields) {
-    const lngLat = await this.mapsAPI.getPostcodeResults(
-      this.state.fields.postcode
-    );
-    const { title, description, bookingEmail } = fields;
+	submissionInProgress(bool) {
+		this.setState({ loading: bool });
+	}
 
-    return {
-      title,
-      description,
-      location: {
-        lat: lngLat.latitude,
-        lon: lngLat.longitude
-      },
-      facilities: this.formatter.formatItemStringToArray(fields.facilities),
-      address: await this.formatter.convertAddressToArray(fields),
-      ownerId: 'testOwnerId',
-      bookingEmail
-    };
-  }
+	async addProperty() {
+		this.submissionInProgress(true);
+		if (
+			this.errorHandler.allFieldsAreCompleted(
+				this.requiredFields,
+				this.state.fields
+			)
+		) {
+			const fields = await this.setUpFieldsObject(this.state.fields);
 
-  cleanFields(fieldsToClean, currentFieldsState) {
-    fieldsToClean.map(fieldName => (currentFieldsState[fieldName] = ''));
-    return currentFieldsState;
-  }
+			this.props.apiClient
+				.post('/properties/', fields)
+				.then(() => {
+                    this.submissionInProgress(false);
+                    const notice = {
+						message: `Property "${fields.title}" successfully added`,
+						style: { color: 'green' }
+					};
+					const currentFieldsState = this.cleanFields(
+						this.allFields,
+						this.state.fields
+					);
+					this.setState({ fields: currentFieldsState, notice });
+				})
+				.catch(error => {
+					console.log(error, fields);
+					const errorMessage = {
+						message: error.toString(),
+						style: { color: 'red' }
+					};
+					this.submissionInProgress(false);
+					this.setState({ notice: errorMessage });
+				});
+		} else {
+			this.handleMissingFields();
+		}
+	}
 
-  submissionInProgress(bool) {
-    this.setState({ loading: bool });
-  }
+	handleMissingFields() {
+		const notice = this.errorHandler.emptyBoxErrorHandler(
+			this.requiredFields,
+			this.state
+		);
+		this.submissionInProgress(false);
+		this.setState({ notice });
+	}
 
-  async addProperty() {
-    this.submissionInProgress(true);
-    if (
-      this.errorHandler.allFieldsAreCompleted(
-        this.requiredFields,
-        this.state.fields
-      )
-    ) {
-      const fields = await this.setUpFieldsObject(this.state.fields);
+	updateInputValue(evt, formName) {
+		let value = evt.target.value.toString();
+		let fields = this.state.fields;
+		fields[formName] = value;
+		this.setState({
+			fields
+		});
+	}
 
-      this.props.apiClient
-        .post('/properties/', fields)
-        .then(() => {
-          const error = {
-            message: `Property "${fields.title}" successfully added`,
-            style: { color: 'green' }
-          };
-          const currentFieldsState = this.cleanFields(
-            this.allFields,
-            this.state.fields
-          );
-          this.submissionInProgress(false);
-          this.setState({ fields: currentFieldsState, error });
-        })
-        .catch(error => {
-          console.log(error, fields);
-          const errorMessage = {
-            message: error.toString(),
-            style: { color: 'red' }
-          };
-          this.submissionInProgress(false);
-          this.setState({ error: errorMessage });
-        });
-    } else {
-      this.handleMissingFields();
-    }
-  }
+	buildForm(fields) {
+		const form = [];
+		const keys = Object.keys(fields);
+		for (let section in keys) {
+			const sectionFields = fields[keys[section]];
+			form.push(
+				<h5 className="center-align">
+					<strong>{keys[section]}</strong>
+				</h5>
+			);
+			for (let field in sectionFields) {
+				form.push(
+					<FormItem
+						name={sectionFields[field].name}
+						label={sectionFields[field].label}
+						value={this.state.fields[sectionFields[field].name]}
+						type={sectionFields[field].type}
+						updateInputValue={this.updateInputValue}
+						key={sectionFields[field].name}
+					/>
+				);
+			}
+		}
+		return form;
+	}
 
-  handleMissingFields() {
-    const error = this.errorHandler.emptyBoxErrorHandler(
-      this.requiredFields,
-      this.state
-    );
-    this.submissionInProgress(false);
-    this.setState({ error });
-  }
-
-  updateInputValue(evt, formName) {
-    let value = evt.target.value.toString();
-    let fields = this.state.fields;
-    fields[formName] = value;
-    this.setState({
-      fields
-    });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="error" style={this.state.error.style} id="error">
-          {this.state.error.message}
-        </div>
-        <h5 className="center-align">
-          <strong>Details</strong>
-        </h5>
-        <FormItem
-          name={'title'}
-          label={'Title'}
-          value={this.state.fields.title}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'description'}
-          type={'textarea'}
-          label={'Property Description'}
-          value={this.state.fields.description}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'facilities'}
-          label={'Facilities (separated by commas)'}
-          value={this.state.fields.facilities}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'bookingEmail'}
-          label={'Booking Email'}
-          type={'email'}
-          value={this.state.fields.bookingEmail}
-          updateInputValue={this.updateInputValue}
-        />
-        <br />
-        <h5 className="center-align">
-          <strong>Address</strong>
-        </h5>
-        <FormItem
-          name={'addressLine1'}
-          label={'Address Line 1'}
-          value={this.state.fields['addressLine1']}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'addressLine2'}
-          label={'Address Line 2 (optional)'}
-          value={this.state.fields['addressLine2']}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'city'}
-          label={'City'}
-          value={this.state.fields['city']}
-          updateInputValue={this.updateInputValue}
-        />
-        <FormItem
-          name={'postcode'}
-          label={'Post Code'}
-          value={this.state.fields['postcode']}
-          updateInputValue={this.updateInputValue}
-        />
-        <br />
-        {this.state.loading === false ? (
-          <Row>
-            <AddPropertyButton addProperty={this.addProperty} />
-            <br />
-          </Row>
-        ) : (
-          <Preloader />
-        )}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="notice" style={this.state.notice.style} id="error">
+					{this.state.notice.message}
+				</div>
+				{this.buildForm(PropertyFields)}
+				<br />
+				{this.state.loading === false ? (
+					<Row>
+						<AddPropertyButton addProperty={this.addProperty} />
+						<br />
+					</Row>
+				) : (
+					<Preloader />
+				)}
+			</div>
+		);
+	}
 }
 
 export default PropertyForm;
